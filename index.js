7cb5a549eab95d003cdf065c60dfb5fd5c995ef4
Michiel@PC <decoster.michiel@gmail.com>
2019-02-13 14:23:36 +0100

    Changed token & added bot online to index.js

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const bot = new Discord.Client({disableEveryone: true});
 
 bot.on("Ready", async() =>{
     console.log(`${bot.user.username} is onlin!`)
+    
+});
+
+bot.on("message", async message => {
+    if(message.author.bot) return;
+    if(message.channel.type === "dm") return;
+    
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
 })
 
 bot.login(botconfig.token);
