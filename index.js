6270ab8c49279e032e06a001ff253322caa3f2a1
Michiel@Laptop <decoster.michiel@gmail.com>
2019-02-14 19:53:52 +0100

    Added memesend & memes folder

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const Discord = require("discord.js");
 const bot = new Discord.Client({disableEveryone: true});
 const greetingsArray = ['Hello', 'Hi', 'Waddup', 'Im enjoying memes', 'Living the goodlife'];
 
+//const memesArray = [images]
+
 bot.on("Ready", async() =>{
-    console.log(`${bot.user.username} is online!`)
+    console.log(`${bot.user.username} is online!`);
     
 });
+
+// bot.emojis("Ready", async() =>{
+//     console.log(`${bot.user.username} is online!`);
+// })
 
 bot.on("message", async message => {
     if(message.author.bot) return;
@@ -25,20 +31,26 @@ bot.on("message", async message => {
         randomGreeting = greetingsArray[theGreeting]
         return message.channel.send(randomGreeting);
     }
-});
-
-bot.on("message", async message =>{
-    if(message.author.bot) return;
-    if(message.channel.type === 'dm') return;
-
-    let prefix = botconfig.prefix;
-    let messageArray = message.content.split(' ');
-    let cmd = messageArray [0];
-    let args = messageArray(1);
-
     if (cmd === `${prefix}emoji`){
         return message.channel.send(':eggplant:');
-        }
-    });
+         }
+    
+    
+    if(cmd === `${prefix}sendmeme`){
+        return message.channel.send('yoink another meme', {
+            files: [
+                'memes/fatmeme.jpg'
+            ]})
+    }
+});
+
+// bot.emojis("message", async message =>{
+//     if(message.author.bot) return;
+//     if(message.channel.type === 'dm') return;
+
+//     let prefix = botconfig.prefix;
+//     let messageArray = message.content.split(' ');
+//     let cmd = messageArray [0];
+//     let args = messageArray(1);
 
 bot.login(botconfig.token)
