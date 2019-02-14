bac25625e3cedfbbfcbdeb3c074f40a3df896908
Michiel@Laptop <decoster.michiel@gmail.com>
2019-02-14 18:32:47 +0100

    Added ?emoji command, not working yet

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 
 const bot = new Discord.Client({disableEveryone: true});
+const greetingsArray = ['Hello', 'Hi', 'Waddup', 'Im enjoying memes', 'Living the goodlife'];
 
 bot.on("Ready", async() =>{
-    console.log(`${bot.user.username} is onlin!`)
+    console.log(`${bot.user.username} is online!`)
     
 });
 
 bot.on("message", async message => {
     if(message.author.bot) return;
-    if(message.channel.type === "dm") return;
+    if(message.channel.type === 'dm') return;
     
     let prefix = botconfig.prefix;
     let messageArray = message.content.split(" ");
@@ -20,8 +21,24 @@ bot.on("message", async message => {
     //?say Hello
 
     if(cmd === `${prefix}hello`){
-        return message.channel.send("Hello!");
+        theGreeting = Math.floor(Math.random()*greetingsArray.length)
+        randomGreeting = greetingsArray[theGreeting]
+        return message.channel.send(randomGreeting);
     }
-})
+});
 
-bot.login(botconfig.token);
+bot.on("message", async message =>{
+    if(message.author.bot) return;
+    if(message.channel.type === 'dm') return;
+
+    let prefix = botconfig.prefix;
+    let messageArray = message.content.split(' ');
+    let cmd = messageArray [0];
+    let args = messageArray(1);
+
+    if (cmd === `${prefix}emoji`){
+        return message.channel.send(':eggplant:');
+        }
+    });
+
+bot.login(botconfig.token)
